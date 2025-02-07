7f904962732a4971981d9d986bf270e2c2c81ca0
vonmarasigan <von.marasigan@ft.com>
2025-02-07 08:42:37 +0000

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,1 +1,18 @@
+import pytest 
+from app import app
+#from <file or module> import <object or class>
 
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_greetme(client):
+    response = client.get('/')
+    assert response.data==b'hello devops'
+    assert response.status_code==200
+
+def test_page(client):
+    response = client.get('/page1')
+    assert response.data==b'welcome to page1'
+    assert response.status_code==200
